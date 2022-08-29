859d13721491409dbaf8c724bb7b6eeb5d6a2633
Elijah-Harvey <potenialtrader@gmail.com>
2022-08-29 16:05:23 -0400

    Implented redux for store/basket

--- App.js
+++ App.js
@@ -6,24 +6,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import {TailwindProvider} from 'tailwindcss-react-native';
 import RestaurantScreen from './screens/RestaurantScreen';
+import {Provider} from 'react-redux';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="Restaurant"
-            component={RestaurantScreen}
-          />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
